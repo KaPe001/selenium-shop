4cf9a022d1cb99b165fd4606c433536b6a626b72
kpetkowska <kingapetkowska@gmail.com>
2021-12-27 08:52:57 +0100

    deleted not used method

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,11 +1,11 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Coordinates;
 import org.openqa.selenium.interactions.Locatable;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.openqa.selenium.support.events.internal.EventFiringMouse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +16,8 @@ import java.util.List;
 public class MainPage extends BasePage {
     ProductGridPage productGridPage = new ProductGridPage(webDriver);
     Logger logger = LoggerFactory.getLogger(MainPage.class);
+    WebDriverEventListener webListener;
 
-    private WebListener webListener;
 
     @FindBy(css = "[id='header'] [id='_desktop_user_info'] .hidden-sm-down")
     WebElement logInBtn;
@@ -100,15 +100,6 @@ public class MainPage extends BasePage {
             newResultList.add(new SingleDropdownResultList(element));
         }
         return newResultList;
-    }
-
-    public String printListProducts() {
-        String printResult = "";
-        List<SingleDropdownResultList> dropdownList = getNewResultList();
-        for (SingleDropdownResultList singleDropdownResultList : dropdownList) {
-            printResult = singleDropdownResultList.getResultFromListName();
-        }
-        return printResult;
     }
 
     public boolean isResultInResultDropDownList(String randomResult) {
